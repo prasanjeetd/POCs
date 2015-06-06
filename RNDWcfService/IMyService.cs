6091c2802515149e050af0f4d3056773850b7764
prasanjeetd <prasanjeetdebnath@rocketmail.com>
2015-06-06 20:30:39 +0530

    Added thw WCF application

---
+++ RNDWcfService/IMyService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RNDWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMyService" in both code and config file together.
+    [ServiceContract]
+    public interface IMyService
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        void MyFunction(MyClass myClass);
+        
+        [OperationContract]
+        MyClass MyFunction1();
+        
+        [OperationContract]
+        MyClass MyFunction2(MyClass myClass);
+        
+    }
+    [DataContract]
+    public class MyClass
+    {
+        //[DataMember]
+        //public string Name { get; set; }
+
+        string name = "No Name";
+        [DataMember]
+        public string Name 
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+}
---
+++ RNDWcfService/MyService.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RNDWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MyService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select MyService.svc or MyService.svc.cs at the Solution Explorer and start debugging.
+    public class MyService : IMyService
+    {
+        public void DoWork()
+        {
+        }
+
+        public void MyFunction(MyClass myClass)
+        {
+            string name = myClass.Name;
+        }
+
+
+        public  MyClass MyFunction1()
+        {
+            MyClass myClass = new MyClass();
+            myClass.Name = "Prasanjeet";
+            return myClass;
+        }
+
+
+        public MyClass MyFunction2(MyClass myClass)
+        {
+            MyClass myClassReturn = new MyClass();
+            myClassReturn.Name = "Prasanjeet" + myClass.Name;
+            return myClassReturn;
+        }
+    }
+}
